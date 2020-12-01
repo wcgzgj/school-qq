1b5f405a69f45594326a433395f6cbcff0f51045
wcgzgj <zhangjia_good@126.com>
2020-12-01 11:20:58 +0800

    项目初始化

---
+++ src/top/faroz/gui/frame/MyFrame.java
@@ -0,0 +1,29 @@
+package top.faroz.gui.frame;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @ClassName MyFrame
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2020/12/1 上午11:07
+ * @Version 1.0
+ **/
+public class MyFrame extends JFrame {
+    public static MyFrame instance=new MyFrame();
+
+    public static MyFrame getInstance() {
+        return instance;
+    }
+
+    private MyFrame() throws HeadlessException {
+        super();
+        this.setLayout(new BorderLayout());
+        this.setBounds(new Rectangle(30,30,400,400));
+        this.setTitle("19180319张健 校园qq");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+}
